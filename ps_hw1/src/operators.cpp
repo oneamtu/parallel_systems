b5c1182420f9800d79329759783626e3d79fbe6b
Octavian Neamțu <octavian.neamtu@annkissam.com>
2020-09-10 22:42:01 -0400

    Update operator per errata.

--- ps_hw1/src/operators.cpp
+++ ps_hw1/src/operators.cpp
@@ -3,10 +3,9 @@
 int __attribute__ ((noinline)) op(int a, int b, int n_loop) {
     volatile int acc = 0;
     for (int i = 0; i < n_loop; i++) {
-        acc = i % 2 == 0 ? (acc + a)/(b+1) : (acc + b)/(a+1);
-    }
-
-    return acc;
+            acc++;
+        }
+    return (a+b)*(acc/n_loop);
 }
 
 
